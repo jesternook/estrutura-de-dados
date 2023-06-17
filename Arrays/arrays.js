946fbcfbfa0b91647ec813379bf8662f428b4459
jesternook <nicocossta@outlook.com>
2023-06-17 14:29:40 -0400

    adição de validações extras nos métodos

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -7,13 +7,35 @@ var ListaDeCompras = /** @class */ (function () {
         this.quantidade = [];
     }
     ListaDeCompras.prototype.adicionarItem = function (nomeProduto, quantidadeDesejada) {
-        this.itens.push(nomeProduto);
-        this.quantidade.push(quantidadeDesejada);
+        if (nomeProduto === "") {
+            console.log("Dado de produto incorreto. Digite o nome do produto");
+        }
+        if (quantidadeDesejada <= 0) {
+            console.log("Dado de quantidade incorreto. Digite a quantidade do produto");
+        }
+        else {
+            this.itens.push(nomeProduto);
+            this.quantidade.push(quantidadeDesejada);
+        }
     };
     ListaDeCompras.prototype.removerItem = function (nomeProduto) {
         var indexProduto = this.itens.indexOf(nomeProduto);
-        this.itens.splice(indexProduto, 1);
-        this.quantidade.splice(indexProduto, 1);
+        for (var _i = 0, _a = this.itens; _i < _a.length; _i++) {
+            var item = _a[_i];
+            if (item != nomeProduto) {
+                console.log("Dado não encontrado. Digite o nome do produto corretamente para remove-lo");
+                return false;
+            }
+            if (nomeProduto === "") {
+                console.log("Dado de produto incorreto. Digite o nome do produto");
+                return false;
+            }
+            else if (item === nomeProduto) {
+                this.itens.splice(indexProduto, 1);
+                this.quantidade.splice(indexProduto, 1);
+                return true;
+            }
+        }
     };
     ListaDeCompras.prototype.listarItens = function () {
         for (var i = 0; i < this.itens.length; i++) {
@@ -29,7 +51,10 @@ novaLista.adicionarItem("Maça", 7);
 novaLista.adicionarItem("Pães", 12);
 novaLista.adicionarItem("Chá de canela", 1);
 novaLista.adicionarItem("Requeijão", 1);
+novaLista.listarItens();
+//dados inválidos
+novaLista.adicionarItem('', -2);
 //remover itens
 novaLista.removerItem('Maça');
-//listar itens
+novaLista.removerItem('paes');
 novaLista.listarItens();
--- Arrays/arrays.ts
+++ Arrays/arrays.ts
@@ -3,17 +3,35 @@ export class ListaDeCompras {
     quantidade: number[] = [];
 
     adicionarItem(nomeProduto: string, quantidadeDesejada: number): any {
-        this.itens.push(nomeProduto);
-        this.quantidade.push(quantidadeDesejada);
+        if (nomeProduto === "") {
+            console.log("Dado de produto incorreto. Digite o nome do produto")
+        } if (quantidadeDesejada <= 0) {
+            console.log("Dado de quantidade incorreto. Digite a quantidade do produto")
+        } else {
+            this.itens.push(nomeProduto);
+            this.quantidade.push(quantidadeDesejada);
+        }
     }
 
     removerItem(nomeProduto: string): any {
         let indexProduto: number = this.itens.indexOf(nomeProduto);
-        this.itens.splice(indexProduto, 1);
-        this.quantidade.splice(indexProduto, 1);
+
+        for (let item of this.itens) {
+            if (item != nomeProduto) {
+                console.log("Dado não encontrado. Digite o nome do produto corretamente para remove-lo");
+                return false
+            } if (nomeProduto === "") {
+                console.log("Dado de produto incorreto. Digite o nome do produto")
+                return false
+            } else if (item === nomeProduto) {
+                this.itens.splice(indexProduto, 1);
+                this.quantidade.splice(indexProduto, 1);
+                return true
+            }
+        }
     }
 
-    listarItens() {
+    listarItens(): any {
         for (let i = 0; i < this.itens.length; i++) {
             console.log(`Item: ${this.itens[i]} -- Quantidade: ${this.quantidade[i]}`)
         }
@@ -27,7 +45,10 @@ novaLista.adicionarItem("Maça", 7);
 novaLista.adicionarItem("Pães", 12);
 novaLista.adicionarItem("Chá de canela", 1)
 novaLista.adicionarItem("Requeijão", 1)
+novaLista.listarItens();
+//dados inválidos
+novaLista.adicionarItem('', -2);
 //remover itens
 novaLista.removerItem('Maça');
-//listar itens
+novaLista.removerItem('paes');
 novaLista.listarItens();
